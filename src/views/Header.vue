62e24a6a772fb5068b99467cacca7855e7dc6a2d
chintensakai <toy_shen@163.com>
2021-05-12 22:33:57 +0800

    chins: portal布局完成

---
+++ src/views/Header.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="menu">
+    <div class="l-content">
+      <el-menu
+        :default-active="activeIndex"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+        router
+      >
+        <el-menu-item index="/">首页</el-menu-item>
+        <el-menu-item index="2">课程</el-menu-item>
+        <el-menu-item index="3">讲师</el-menu-item>
+        <el-menu-item index="4">关于</el-menu-item>
+      </el-menu>
+    </div>
+    <div class="r-content">
+      <el-dropdown trigger="click">
+        <span class="el-dropdown-link">
+          <el-avatar
+            src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+          ></el-avatar>
+        </span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>个人中心</el-dropdown-item>
+          <el-dropdown-item>退出</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      activeIndex: "1",
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
+};
+</script>
+<style>
+.menu {
+  display: flex;
+  height: 100%;
+  justify-content: space-between;
+  align-items: center;
+}
+.l-content {
+  width: 100%;
+}
+.menu ul {
+  display: flex;
+  justify-content: center;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,134 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>热门课程</h3>
+    <el-row :gutter="20" type="flex" justify="center">
+      <el-col :span="6"
+        ><div class="grid-content" @click="gotoCourseInfo(curseId)">
+          <el-card :body-style="{ padding: '0px' }" shadow="hover">
+            <img
+              src="https://www.gulixueyuan.com/files/default/2018/06-15/0918251905f6057842.jpg"
+              class="image"
+            />
+            <div style="padding: 14px">
+              <span>好吃的汉堡</span>
+              <div class="bottom clearfix">
+                <time class="time">{{ currentDate }}</time>
+                <el-button type="text" class="button">操作按钮</el-button>
+              </div>
+            </div>
+          </el-card>
+        </div></el-col
+      >
+      <el-col :span="6"
+        ><div class="grid-content">
+          <el-card :body-style="{ padding: '0px' }" shadow="hover">
+            <img
+              src="https://www.gulixueyuan.com/files/default/2018/06-15/0918146150a9565117.jpg"
+              class="image"
+            />
+            <div style="padding: 14px">
+              <span>好吃的汉堡</span>
+              <div class="bottom clearfix">
+                <time class="time">{{ currentDate }}</time>
+                <el-button type="text" class="button">操作按钮</el-button>
+              </div>
+            </div>
+          </el-card>
+        </div></el-col
+      >
+      <el-col :span="6"
+        ><div class="grid-content">
+          <el-card :body-style="{ padding: '0px' }" shadow="hover">
+            <img
+              src="https://www.gulixueyuan.com/files/default/2018/06-14/1709051b4c3c309882.jpg"
+              class="image"
+            />
+            <div style="padding: 14px">
+              <span>好吃的汉堡</span>
+              <div class="bottom clearfix">
+                <time class="time">{{ currentDate }}</time>
+                <el-button type="text" class="button">操作按钮</el-button>
+              </div>
+            </div>
+          </el-card>
+        </div></el-col
+      >
+      <el-col :span="6"
+        ><div class="grid-content">
+          <el-card :body-style="{ padding: '0px' }" shadow="hover">
+            <img
+              src="https://www.gulixueyuan.com/files/course/2020/09-17/092058a40481921036.jpg"
+              class="image"
+            />
+            <div style="padding: 14px">
+              <span>好吃的汉堡</span>
+              <div class="bottom clearfix">
+                <time class="time">{{ currentDate }}</time>
+                <el-button type="text" class="button">操作按钮</el-button>
+              </div>
+            </div>
+          </el-card>
+        </div></el-col
+      >
+    </el-row>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      currentDate: new Date()
+    };
+  },
+  methods: {
+    gotoCourseInfo(courseId) {
+      alert(courseId)
+      this.$router.push("/about")
+    }
   }
-}
+};
 </script>
+
+<style>
+.el-row {
+  margin-bottom: 20px;
+}
+.el-col {
+  border-radius: 4px;
+}
+
+.time {
+  font-size: 13px;
+  color: #999;
+}
+
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
+
+.button {
+  padding: 0;
+  float: right;
+}
+
+.image {
+  width: 100%;
+  display: block;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+
+.clearfix:after {
+  clear: both;
+}
+</style>
